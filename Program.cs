5a54c8297444f6a2aaa4e8ec99e2a1d62df2f95b
Michael Trevino <mtrevin93@gmail.com>
2021-09-09 12:19:41 -0400

    order by rating, select name

--- Program.cs
+++ Program.cs
@@ -120,7 +120,7 @@ namespace NinetiesTV
         // 13. Return the names of the comedy shows sorted by IMDB rating.
         static List<string> ComediesByRating(List<Show> shows)
         {
-            throw new NotImplementedException();
+            return shows.OrderByDescending(s => s.ImdbRating).Select(s => s.Name).ToList();
         }
 
         // 14. Return the shows with more than one genre ordered by their starting year.
